06b9208f6c8cf6f5f4a1e8fa4f11cc8e9812510c
Art <kevlar101@mail.ru>
2025-05-17 16:55:36 +0300

    credentials, signIn, signOut

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,61 @@
 import NextAuth from "next-auth"
 import authConfig from "@/auth.config"
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes
+
+} from "@/routes";
 
 const {auth} = NextAuth(authConfig)
 
 export default auth((req) => {
   const isLoggedIn = !!req.auth;
-  console.log("ROUTE: ", req.nextUrl.pathname);
-  console.log("IS LOGGEDIN: ", isLoggedIn)
+  const {nextUrl} = req;
+  
+
+  /**
+   * console.log(nextUrl)
+   * {
+  href: 'http://localhost:3000/auth/login',
+  origin: 'http://localhost:3000',
+  protocol: 'http:',
+  username: '',
+  password: '',
+  host: 'localhost:3000',
+  hostname: 'localhost',
+  port: '3000',
+  pathname: '/auth/login',
+  search: '',
+  searchParams: URLSearchParams {  },
+  hash: ''
+}
+   */
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+  if(isApiAuthRoute){
+    return null;
+  }
+
+  if(isAuthRoute){
+    if(isLoggedIn){
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+    }
+    return null;
+  }
+
+  if(!isLoggedIn || !isPublicRoute){
+    return Response.redirect(new URL("/auth/login", nextUrl))
+  }
+
+  return null;
 });
+
+
 
 export const config = { 
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']
